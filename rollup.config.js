86dea6efd54d3ace818979e48e33b1aa38faae42
Matt Light <lightster@gmail.com>
2024-09-07 13:48:07 -0700

    Reduce file size further

--- rollup.config.js
+++ rollup.config.js
@@ -1,14 +1,19 @@
+import terser from "@rollup/plugin-terser";
 import typescript from "@rollup/plugin-typescript";
 import { nodeResolve } from "@rollup/plugin-node-resolve";
 import copy from "rollup-plugin-copy";
+import kontra from "rollup-plugin-kontra";
 import serve from "rollup-plugin-serve";
+
+const isWatching =
+  process.argv.includes("-w") || process.argv.includes("--watch");
 
 export default [
   {
     input: "src/main.ts",
     output: {
       file: "build/index.js",
-      format: "cjs",
+      format: "iife",
     },
     plugins: [
       copy({
@@ -20,7 +25,17 @@ export default [
       }),
       typescript(),
       nodeResolve(),
-      serve("build"),
+      kontra({
+        sprite: { animation: true },
+        tileEngine: {
+          camera: true,
+          query: true,
+          tiled: true,
+        },
+        debug: isWatching,
+      }),
+      isWatching && serve("build"),
+      !isWatching && terser(),
     ],
   },
 ];
